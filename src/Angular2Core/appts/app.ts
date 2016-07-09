ff982785f3b1b550872586dc8c1a1a9b2755d7d3
Mehdi Lahlou <lahlou.mehdi@gmail.com>
2016-07-09 15:36:18 +0200

    first commit

---
+++ src/Angular2Core/appts/app.ts
@@ -0,0 +1,12 @@
+﻿import {Component} from '@angular/core';
+import {HomeComponent} from './components/home.component';
+
+@Component({
+    selector: 'my-app',
+    template: '<home></home>',
+    directives: [HomeComponent]
+})
+
+export class AppComponent {
+
+}
---
+++ src/Angular2Core/appts/services/angularcore.service.ts
@@ -0,0 +1,29 @@
+﻿import { Injectable }    from '@angular/core';
+import { Http, Headers } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+
+@Injectable()
+export class AngularCoreService {
+
+    private avrUrl = 'api';  // URL to web api
+
+    constructor(private http: Http) {
+
+    }
+
+    getData(id: number): Promise<string[]> {
+        var data = localStorage.getItem('data');
+        return Promise.resolve(["toto", data]);
+    }
+
+    storeData(json: string): Promise<string[]> {
+        localStorage.setItem('data', json);
+        return Promise.resolve(["toto", "titi"]);
+    }
+    private handleError(error: any) {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
+    }
+}
